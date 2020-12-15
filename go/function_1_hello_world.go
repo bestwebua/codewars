d302ec573c9271095aca315557acdc5eb5324767
Vladislav Trotsenko <admin@bestweb.com.ua>
2020-12-15 22:26:54 +0200

    Complete 8kyu kata

---
+++ go/function_1_hello_world.go
@@ -0,0 +1,10 @@
+// Function 1 - hello world (8kyu)
+// https://www.codewars.com/kata/523b4ff7adca849afe000035/go
+//
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+package kata
+
+func greet() string {
+	return "hello world!"
+}
