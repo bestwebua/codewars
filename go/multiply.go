2aee6c319637cc7561fa23daa068ed3434ded758
Vladislav Trotsenko <admin@bestweb.com.ua>
2020-12-16 09:49:34 +0200

    Complete 8kyu kata

---
+++ go/multiply.go
@@ -0,0 +1,10 @@
+// Multiply (8kyu)
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/go
+
+// This code does not execute properly. Try to figure out why.
+
+package multiply
+
+func Multiply(a, b int) int {
+	return a * b
+}
