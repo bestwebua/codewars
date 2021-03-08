4ea7ac3a8ec5871ad706773212ebbdde7c6a288f
Vladislav Trotsenko <admin@bestweb.com.ua>
2021-03-08 20:58:02 +0200

    Complete 8kyu kata

---
+++ js/reversed_strings.js
@@ -0,0 +1,9 @@
+// Reversed Strings, 8 kyu
+// https://www.codewars.com/kata/reversed-strings
+
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+
+function solution(str){
+  return str.split('').reverse().join('');
+}
