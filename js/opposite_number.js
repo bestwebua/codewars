b1d4b939ae93653c74f2c334667e7a95792595b9
Vladislav Trotsenko <admin@bestweb.com.ua>
2021-03-08 21:06:34 +0200

    Complete 8kyu kata

---
+++ js/opposite_number.js
@@ -0,0 +1,13 @@
+// Opposite number, 8 kyu
+// https://www.codewars.com/kata/opposite-number
+
+// Very simple, given a number, find its opposite.
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  return -number;
+}
